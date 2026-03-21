afd6e578f786d9655f85f1dc7f13393128842d00
calenfretts <calenfretts@users.noreply.github.com>
2026-03-21 20:01:14 +0000

    deploy: cosmosive/cosmosive@5ad0ee5d9bdaf4c451d2e54c0693030b6dc2e1d7

---
+++ assets/ephemerisBatchSource-DoRGC1XV.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CgBxV8u7.js";import{fetchEphemerisBatch as r}from"./horizons-Bqd4STu1.js";const a=async t=>{const s=o(t.startJd),c=o(t.endJd);return{byBodyId:await r({targetIds:[...t.bodyIds],start:s,stop:c,step:t.step,concurrency:t.concurrency??2})}};export{a as defaultEphemerisBatchSource};
