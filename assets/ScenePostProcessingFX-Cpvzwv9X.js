efd945c02b8f997e94e2100815e2a7f1c0ce59c9
calenfretts <calenfretts@users.noreply.github.com>
2026-03-22 21:34:55 +0000

    deploy: cosmosive/cosmosive@b520a51ea551a4c1e923885cd02faa83e65b21f2

---
+++ assets/ScenePostProcessingFX-Cpvzwv9X.js
@@ -0,0 +1,1 @@
+import{j as s}from"./react-vendor-ebl2-8GA.js";import{E as e,j as o,k as n}from"./three-vendor-Bia0WF0B.js";function r(){return s.jsxs(e,{children:[s.jsx(o,{luminanceThreshold:.2,luminanceSmoothing:.9,intensity:1.2}),s.jsx(n,{eskil:!1,offset:.2,darkness:.6})]})}export{r as ScenePostProcessingFX};
