31b1e13d64ef27b13b9d3266b2067f71535bfee6
calenfretts <calenfretts@users.noreply.github.com>
2026-05-06 23:22:17 +0000

    deploy: cosmosive/cosmosive@3a13d4190a52eef3ace80ae8aa904006b4c9ac5f

--- assets/NebulaVolumeMountR3f-BPShuao6.js
+++ assets/NebulaVolumeMountR3f-BPShuao6.js
@@ -1,4 +1,4 @@
-import{i as B}from"./rolldown-runtime-BOC55nOF.js";import{p as I}from"./r3f-vendor-D47J47SK.js";import{n as O}from"./state-data-vendor-yuKHXx2_.js";import{R as x,V as q,bt as p,dt as j,lt as Q,p as b,w as E,yt as V}from"./three-vendor-BZgPQKFQ.js";import{La as M,On as k,a as z,go as W,ma as N}from"./index-jP5TTIHQ.js";import{a as G,i as Y,o as H,t as X}from"./r3fHostExtras-DQA4DyBd.js";import{n as K,t as R}from"./queryCacheNotify-DhrxQOFG.js";var _=B(I(),1),C=220,J=5e4,w=90,$=28e3,Z=10,h=10,ee=17,ae=9,oe=`
+import{i as B}from"./rolldown-runtime-BOC55nOF.js";import{p as I}from"./r3f-vendor-D47J47SK.js";import{n as O}from"./state-data-vendor-yuKHXx2_.js";import{R as x,V as q,bt as p,dt as j,lt as Q,p as b,w as E,yt as V}from"./three-vendor-BZgPQKFQ.js";import{La as M,On as k,a as z,go as W,ma as N}from"./index-BAqUZYFZ.js";import{a as G,i as Y,o as H,t as X}from"./r3fHostExtras-DQA4DyBd.js";import{n as K,t as R}from"./queryCacheNotify-BJJS8o1U.js";var _=B(I(),1),C=220,J=5e4,w=90,$=28e3,Z=10,h=10,ee=17,ae=9,oe=`
 varying vec3 vWorldPos;
 varying vec3 vLocalPos;
 varying vec3 vNormalDir;
