87a65944c61dc774dce260bf3abe38aabcd19c9e
calenfretts <calenfretts@users.noreply.github.com>
2026-05-13 17:49:31 +0000

    deploy: cosmosive/cosmosive@4e945ae26946770c19ff8adfffa0e5996ea32a21

---
+++ assets/dsoMajorCluster-C8sGnn-C.js
@@ -0,0 +1,1 @@
+import{n as r,t as e}from"./majorDsoSelector-CXO4P3rt.js";var a=new Set(["star_cluster"]);function s(t){return r(t,{types:a,max:5})}function n(t){return e(s(t))}export{s as n,n as t};
