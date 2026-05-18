faea003bb56c2651f4aab98782dbd98f5165477d
calenfretts <calenfretts@users.noreply.github.com>
2026-05-18 06:27:57 +0000

    deploy: cosmosive/cosmosive@528a2c0f2adb7f4566b98d413ee49c2e9fd98d74

---
+++ assets/useGameplayHost-uYjxGaIh.js
@@ -0,0 +1,1 @@
+import{i as a}from"./rolldown-runtime-BOC55nOF.js";import{p as o}from"./r3f-vendor-DwzEju3x.js";import{n as r,t as s}from"./activeGameplayHost-DJPl5OkW.js";var e=a(o(),1),m=(0,e.createContext)(null);function c(){const t=(0,e.useContext)(m)??s();return r(t),t}export{m as n,c as t};
